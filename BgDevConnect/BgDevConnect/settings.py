a394eb6d7d0974d92d0b103adcdf8d0f982d1b14
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-11-01 10:36:18 +0200

    add: all apps created + templates and css

--- BgDevConnect/BgDevConnect/settings.py
+++ BgDevConnect/BgDevConnect/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "authentication.apps.AuthenticationConfig",
+    "content_management.apps.ContentManagementConfig",
+    "public.apps.PublicConfig",
+    "user_content.apps.UserContentConfig"
 ]
 
 MIDDLEWARE = [
@@ -117,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
